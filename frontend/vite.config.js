a7ee8d3ebe4b1a72cc984aca8c2b4a52d12c4f35
Voktor Vovk <vivat.bbb@gmail.com>
2025-04-21 21:07:03 +0300

    - fixed small problems

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,19 +2,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  // base: './',
   plugins: [vue()],
   build: {
     outDir: 'dist',
   },
   server: {
     proxy: {
-      // Прокси для API-запросов
       '/api': {
-        target: 'http://localhost:3000', // URL вашего бэкенда
-        changeOrigin: true, // Менять Origin на целевой URL
-        secure: false, // Используйте false, если сервер работает через HTTP
-        // rewrite: (path) => path.replace(/^\/api/, ''), // Удаляет /api из пути
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
